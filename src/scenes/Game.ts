0438ea1f04d4f0d1838ff2487cc7d85ab14f110a
shawnbmiller <shawnbrandonmiller@gmail.com>
2024-10-19 15:54:28 -0400

    accounting

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -2,7 +2,7 @@ import { Scene } from 'phaser';
 import WebFont from 'webfontloader';
 import { Deck } from '../models/deck';
 import { PlayingCard, Rank, Suit } from '../models/playing-card';
-import { PokerHandEvaluator } from '../models/poker-hand-evaluator';
+import { PokerHand, PokerHandEvaluator } from '../models/poker-hand-evaluator';
 
 
 const cardWidth = 190;
@@ -10,10 +10,36 @@ const cardHeight = cardWidth / 2.5 * 3.5;
 const cardSpacing = 10;
 const startX = 512 - ((cardWidth + cardSpacing) * 2);
 const startY = 384 + (cardHeight / 2) + cardSpacing;
-const payouts = {
-    'Royal Flush': 250, 'Str Flush': 50, 'Four of a Kind': 25, 'Full House': 6, 'Flush': 5,
-    'Straight': 4, '3 of a Kind': 3, '2 Pair': 2, 'Jacks or Better': 1
-};
+// const payouts = {
+//     'Royal Flush': 250, 'Str Flush': 50, 'Four of a Kind': 25, 'Full House': 6, 'Flush': 5,
+//     'Straight': 4, '3 of a Kind': 3, '2 Pair': 2, 'Jacks or Better': 1
+// };
+
+// map poker hand to payout
+const payoutMap = new Map<PokerHand, number>([
+    [PokerHand.RoyalFlush, 250],
+    [PokerHand.StraightFlush, 50],
+    [PokerHand.FourOfAKind, 25],
+    [PokerHand.FullHouse, 6],
+    [PokerHand.Flush, 5],
+    [PokerHand.Straight, 4],
+    [PokerHand.ThreeOfAKind, 3],
+    [PokerHand.TwoPair, 2],
+    [PokerHand.JacksOrBetter, 1]
+]);
+
+// map poker hand to display text
+const handTextMap = new Map<PokerHand, string>([
+    [PokerHand.RoyalFlush, 'Royal Flush'],
+    [PokerHand.StraightFlush, 'Str Flush'],
+    [PokerHand.FourOfAKind, '4 of a Kind'],
+    [PokerHand.FullHouse, 'Full House'],
+    [PokerHand.Flush, 'Flush'],
+    [PokerHand.Straight, 'Straight'],
+    [PokerHand.ThreeOfAKind, '3 of a Kind'],
+    [PokerHand.TwoPair, '2 Pair'],
+    [PokerHand.JacksOrBetter, 'Jacks or Better']
+]);
 
 export class Game extends Scene {
     camera: Phaser.Cameras.Scene2D.Camera;
@@ -27,6 +53,9 @@ export class Game extends Scene {
     deck: Deck;
     hand: PlayingCard[] = [];
     handText: Phaser.GameObjects.Text;
+    credits: number;
+    bet: number;
+    creditsText: Phaser.GameObjects.Text;
 
 
 
@@ -35,10 +64,12 @@ export class Game extends Scene {
         this.suit = ['hearts', 'diamonds', 'clubs', 'spades'];
         this.values = ['ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king'];
         this.fileNames = this.suit.map(s => this.values.map(v => `${s}_${v}`)).flat();
+        this.credits = 100;
+        this.bet = 1;
+
     }
 
     init() {
-        //  Inject our CSS
         const element = document.createElement('style');
 
         document.head.appendChild(element);
@@ -64,10 +95,7 @@ export class Game extends Scene {
                 this.setupGameInterface();
             }
         });
-
     }
-
-
 
     private setupGameInterface() {
         this.add.text(512, 70, 'BONUS POKER', { fontFamily: 'upheaval', fontSize: '32pt', color: '#ffffff', shadow: { color: '#333333', offsetX: 2, offsetY: 2, blur: 0, fill: true, stroke: false } }).setOrigin(0.5);
@@ -105,9 +133,9 @@ export class Game extends Scene {
 
         const style2: Phaser.Types.GameObjects.Text.TextStyle = { fontFamily: 'upheaval', fontSize: '24pt', color: '#ffff66', shadow: { color: '#000000', offsetX: 1, offsetY: 1, blur: 0, fill: true, stroke: false } };
         let y = 0;
-        for (const [key, value] of Object.entries(payouts)) {
+        for (const [key, value] of payoutMap.entries()) {
             const box = y > 5 ? payoutBox2 : payoutBox1;
-            this.add.text(0, y, `${key.toUpperCase()}`, style2).setOrigin(0, 0).setPosition(box.inner.x - box.inner.width / 2 + 5, box.inner.y - box.inner.height / 2 + (y % 6) * 30);
+            this.add.text(0, y, `${handTextMap.get(key)}`, style2).setOrigin(0, 0).setPosition(box.inner.x - box.inner.width / 2 + 5, box.inner.y - box.inner.height / 2 + (y % 6) * 30);
             this.add.text(0, y, `${value}`, style2).setOrigin(1, 0).setPosition(box.valuesBox.x + box.valuesBox.width / 2 - 5, box.valuesBox.y - box.valuesBox.height / 2 + (y % 6) * 30);
             y++;
         }
@@ -115,11 +143,11 @@ export class Game extends Scene {
 
     private setupCreditsAndBet() {
         this.add.text(1024 - 10, 768 - 50, 'CREDITS', { fontFamily: 'upheaval', fontSize: '24pt', color: '#ffffff', shadow: { color: '#333333', offsetX: 2, offsetY: 2, blur: 0, fill: true, stroke: false } }).setOrigin(1, 1);
-        this.add.text(1024 - 10, 768 - 20, '100', { fontFamily: 'upheaval', fontSize: '24pt', color: '#ffffff', shadow: { color: '#333333', offsetX: 2, offsetY: 2, blur: 0, fill: true, stroke: false } }).setOrigin(1, 1);
+        this.creditsText = this.add.text(1024 - 10, 768 - 20, this.credits.toString(), { fontFamily: 'upheaval', fontSize: '24pt', color: '#ffffff', shadow: { color: '#333333', offsetX: 2, offsetY: 2, blur: 0, fill: true, stroke: false } }).setOrigin(1, 1);
 
         const betLabel = this.add.text(0, 0, 'BET', { fontFamily: 'upheaval', fontSize: '24pt', color: '#ffffff', shadow: { color: '#333333', offsetX: 2, offsetY: 2, blur: 0, fill: true, stroke: false } }).setOrigin(1, 1);
         Phaser.Display.Align.In.TopRight(betLabel, this.card[4], -15, betLabel.height + 5);
-        const betValue = this.add.text(0, 0, '1', { fontFamily: 'upheaval', fontSize: '24pt', color: '#ffffff', shadow: { color: '#333333', offsetX: 2, offsetY: 2, blur: 0, fill: true, stroke: false } }).setOrigin(1, 1);
+        const betValue = this.add.text(0, 0, this.bet.toString(), { fontFamily: 'upheaval', fontSize: '24pt', color: '#ffffff', shadow: { color: '#333333', offsetX: 2, offsetY: 2, blur: 0, fill: true, stroke: false } }).setOrigin(1, 1);
         Phaser.Display.Align.In.TopRight(betValue, this.card[4], -15, 0);
     }
 
@@ -131,31 +159,10 @@ export class Game extends Scene {
         drawButton.on('pointerdown', () => {
             if (this.state === GameState.Bet) {
                 // perform Initial draw
-                this.heldText.forEach(held => held.setVisible(false));
-                this.handText.setVisible(false);
-                this.deck = new Deck();
-                this.deck.shuffle();
-                this.hand = [];
-                for (let i = 0; i < 5; i++) {
-                    this.hand.push(this.deck.deal());
-                }
-                for (let i = 0; i < 5; i++) {
-                    this.time.delayedCall(i * 200, () => {
-                        this.sound.play('wood');
-                        this.card[i].setTexture(this.getCardTexture(this.hand[i]));
-                        if (i == 4) {
-                            this.state = GameState.InitialDraw;
-                            const evaluator = new PokerHandEvaluator(this.hand);
-                            const evaluation = evaluator.evaluate();
-                            this.handText.setText(`${evaluation}`);
-                            this.handText.setVisible(true);
-                            Phaser.Display.Align.In.Center(this.handText, this.heldText[2], 0, -this.handText.height);
-                            console.log("initial hand",...this.hand)
-                        }
-                    });
-                }
+                this.startGameRound();
             } else if (this.state === GameState.InitialDraw) {
                 // perform Redraw
+                this.state = GameState.Redraw
                 let count = 0;
                 for (let i = 0; i < 5; i++) {
                     // get a count of cards that are not held
@@ -169,21 +176,32 @@ export class Game extends Scene {
                         });
                         count++;
                     }
-
                 }
+
+                // evaluate the hand after all redraws
                 this.time.delayedCall(count * 200, () => {
-                    this.state = GameState.Redraw
+                    // clear hold status
+                    this.hand.forEach(card => card.held = false);
+                    this.heldText.forEach(held => held.setVisible(false));
+
+
                     const evaluator = new PokerHandEvaluator(this.hand);
-                    console.log("redraw hand",...this.hand)
+                    console.log("redraw hand", ...this.hand)
                     const evaluation = evaluator.evaluate();
                     this.handText.setText(`${evaluation}`);
                     Phaser.Display.Align.In.Center(this.handText, this.heldText[2], 0, -this.handText.height);
                     this.handText.setVisible(true);
-                    // clear hold status
-                    this.hand.forEach(card => card.held = false);
-                    this.heldText.forEach(held => held.setVisible(false));
+                    
+                    // calculate payout
+                    const payout = payoutMap.get(evaluation);
+                    if (payout !== undefined) {
+                        this.credits += payout * this.bet;
+                    }
+
+                    this.displayCredits();
+
                 });
-                
+
             }
             else {
                 // set all cards to face down
@@ -196,6 +214,43 @@ export class Game extends Scene {
                 this.state = GameState.Bet;
             }
 
+        });
+    }
+
+    private startGameRound() {
+        // subtract bet from credits
+        this.credits -= this.bet;
+        this.displayCredits();
+
+        // reset UI
+        this.heldText.forEach(held => held.setVisible(false));
+        this.handText.setVisible(false);
+
+        // get a new deck, shuffle, and deal 5 cards
+        this.deck = new Deck();
+        this.deck.shuffle();
+        this.hand = [];
+        for (let i = 0; i < 5; i++) {
+            this.hand.push(this.deck.deal());
+        }
+
+        // display the cards
+        for (let i = 0; i < 5; i++) {
+            this.time.delayedCall(i * 200, () => {
+                this.sound.play('wood');
+                this.card[i].setTexture(this.getCardTexture(this.hand[i]));
+            });
+        }
+
+        // after all cards are displayed, evaluate the hand
+        this.time.delayedCall(5 * 200, () => {
+            this.state = GameState.InitialDraw;
+            const evaluator = new PokerHandEvaluator(this.hand);
+            const evaluation = evaluator.evaluate();
+            this.handText.setText(`${evaluation}`);
+            this.handText.setVisible(true);
+            Phaser.Display.Align.In.Center(this.handText, this.heldText[2], 0, -this.handText.height);
+            console.log("initial hand", ...this.hand);
         });
     }
 
@@ -303,7 +358,10 @@ export class Game extends Scene {
         return new Command(() => this.toggleHold(card), () => true);
     }
 
-
+    private displayCredits() {
+        this.creditsText.setText(this.credits.toString());
+    }
+    
     // a command to draw new cards
 }
 
